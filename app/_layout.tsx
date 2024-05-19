76f4f8e02ed2ff4d8fa55760db0bffe61302a315
kohama66 <54892265+kohama66@users.noreply.github.com>
2024-05-19 16:11:35 +0900

    feat: add toast

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,15 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
+import Toast from "react-native-toast-message";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -31,16 +25,13 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
-    // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-    //   <Stack>
-    //     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    //     <Stack.Screen name="+not-found" />
-    //   </Stack>
-    // </ThemeProvider>
+    <>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+      <Toast />
+    </>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,15 @@
 import Calender from "@/components/Calender";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { FontAwesome } from "@expo/vector-icons";
+import { Button, StyleSheet, View } from "react-native";
 import * as Clipboard from "expo-clipboard";
 import { newScheduleDays, ScheduleDay } from "@/utils/scheduleDays";
+import { MonthSelector } from "@/components/MonthSelector";
+import Toast from "react-native-toast-message";
 
 export default function HomeScreen() {
   const now = new Date();
   const [month, setMonth] = useState(now.getMonth() + 1);
   const [result, setResult] = useState<ScheduleDay[]>(newScheduleDays(month));
-
-  const changeMonth = (delta: number) => {
-    setMonth((prevMonth) => {
-      let newMonth = prevMonth + delta;
-      if (newMonth < 1) {
-        newMonth = 12;
-      } else if (newMonth > 12) {
-        newMonth = 1;
-      }
-      return newMonth;
-    });
-  };
 
   const handleSetResult = (value: ScheduleDay[]) => {
     setResult(value);
@@ -33,24 +22,16 @@ export default function HomeScreen() {
 
     const text = month + "月\n" + daysText;
     Clipboard.setStringAsync(text);
+    Toast.show({
+      type: "success",
+      text1: "コピーが完了しました",
+      text2: "貼り付けて使用してください 👋",
+    });
   };
 
   return (
     <View style={styles.baseContainer}>
-      <View style={styles.monthContainer}>
-        <TouchableOpacity onPress={() => changeMonth(-1)}>
-          <FontAwesome name="caret-left" size={70} color="blue" />
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.month}>
-          <Text style={styles.monthLabel}>{month.toString()}</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => changeMonth(1)}>
-          <FontAwesome name="caret-right" size={70} color="blue" />
-        </TouchableOpacity>
-      </View>
-
+      <MonthSelector month={month} setMonth={setMonth} />
       <Calender month={month} handleResult={handleSetResult} />
 
       <View style={styles.submitWrapper}>
@@ -101,78 +82,3 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
   },
 });
-
-// import { Image, StyleSheet, Platform } from "react-native";
-
-// import { HelloWave } from "@/components/HelloWave";
-// import ParallaxScrollView from "@/components/ParallaxScrollView";
-// import { ThemedText } from "@/components/ThemedText";
-// import { ThemedView } from "@/components/ThemedView";
-
-// export default function HomeScreen() {
-//   return (
-//     <ParallaxScrollView
-//       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-//       headerImage={
-//         <Image
-//           source={require("@/assets/images/partial-react-logo.png")}
-//           style={styles.reactLogo}
-//         />
-//       }
-//     >
-//       <ThemedView style={styles.titleContainer}>
-//         <ThemedText type="title">Welcome!</ThemedText>
-//         <HelloWave />
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-//         <ThemedText>
-//           Edit{" "}
-//           <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-//           to see changes. Press{" "}
-//           <ThemedText type="defaultSemiBold">
-//             {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-//           </ThemedText>{" "}
-//           to open developer tools.
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-//         <ThemedText>
-//           Tap the Explore tab to learn more about what's included in this
-//           starter app.
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-//         <ThemedText>
-//           When you're ready, run{" "}
-//           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-//           to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-//           directory. This will move the current{" "}
-//           <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-//           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-//         </ThemedText>
-//       </ThemedView>
-//     </ParallaxScrollView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   titleContainer: {
-//     flexDirection: "row",
-//     alignItems: "center",
-//     gap: 8,
-//   },
-//   stepContainer: {
-//     gap: 8,
-//     marginBottom: 8,
-//   },
-//   reactLogo: {
-//     height: 178,
-//     width: 290,
-//     bottom: 0,
-//     left: 0,
-//     position: "absolute",
-//   },
-// });
